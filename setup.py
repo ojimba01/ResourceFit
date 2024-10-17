a994f59624a439c71d897e687cc612df87afd12b
Olayinka Jimba <ojimba01@gmail.com>
2024-10-17 12:38:51 -0400

    general dist clean up after release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="resourcefit",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         'psutil>=5.9.5',
